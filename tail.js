b838e0eef62ae648ff89b2f8038e71b7cb2bc663
Kyle Lemmon <kylej.lemmon@gmail.com>
2021-02-02 00:21:52 +0000

    assert equals array tail function

---
+++ tail.js
@@ -0,0 +1,33 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+const tail = function(arr) {
+
+  let tailing = [];
+  for (let i = 0; i < arr.length; i++) {
+
+    if (i > 0) {
+      tailing.push(arr[i]);
+    }
+  }
+
+
+  return tailing;
+};
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3);
+
+const empty = [];
+assertEqual(tail(empty).length,0);
+
+const single = 'single';
+assertEqual(tail(single.length).length, 0);
