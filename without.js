fe9cdeb4acce11f5d1d611a6ffcdebd40d81c6c2
Kyle Lemmon <kylelemmon@Kyles-MacBook-Air.local>
2021-02-02 21:24:08 -0800

    removed comments from file

--- without.js
+++ without.js
@@ -57,6 +57,5 @@ console.log(without([1, 2, 3], [1,3]));
 console.log(without(["1", "LHL", "3"], ['1', "2", "3"]));
 
 const words = ["hello", "world", "lighthouse"];
-without(words, ["lighthouse"]); // no need to capture return value for this test case
-// Make sure the original array was not altered by the without function
+without(words, ["lighthouse"]); 
 assertArrayEquals(words, ["hello", "world", "lighthouse"]);
