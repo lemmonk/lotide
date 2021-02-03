6a62bde7ada517043c13d28f9974ebe46d18e23f
Kyle Lemmon <kylej.lemmon@gmail.com>
2021-02-03 00:17:31 +0000

    assert compare arrays

---
+++ assertArraysEqual.js
@@ -0,0 +1,32 @@
+const eqArrays = function(arr1, arr2) {
+
+  for (let i = 0; i < arr1.length; i++) {
+
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+
+  }
+
+  return true;
+
+};
+
+
+const assertArrayEquals = function(actual, expected) {
+
+  const compare = eqArrays(actual,expected);
+
+
+  if (compare) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+
+};
+
+assertArrayEquals([1,2,3],[1,2,3]);
+assertArrayEquals([1,2,'3'],[1,2,3]);
+assertArrayEquals([1,2,3],[1,4,3]);
