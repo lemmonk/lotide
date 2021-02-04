f40da361b3189e3fe72829c56b05523e305a3806
Kyle Lemmon <kylelemmon@Kyles-MacBook-Air.local>
2021-02-03 17:42:29 -0800

    find key by value of an object

---
+++ findKeyByValue.js
@@ -0,0 +1,37 @@
+
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+
+
+
+const findKeyByValue = function(object, value) {
+
+  for (let obj in object) {
+    
+    if (object[obj] === value) {
+      return obj;
+    }
+
+    
+  }
+  return undefined;
+};
+
+
+const bestTVShowsByGenre = {
+  sciFi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
